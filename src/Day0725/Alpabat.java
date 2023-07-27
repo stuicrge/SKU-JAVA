56783b85013de1adbb860659b5d43d85a9697fb4
김재훈 <wosl1006@naver.com>
2023-07-27 22:17:44 +0900

    push 0726

---
+++ src/Day0725/Alpabat.java
@@ -0,0 +1,23 @@
+package Day0725;
+
+public class Alpabat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		StringBuilder stringbuilder = new StringBuilder();
+		
+		for(char i='a';i<='z';i++) {
+			
+			stringbuilder.append(i);
+			
+		}
+		
+		System.out.println(stringbuilder);
+		
+		
+		
+	}
+
+}
