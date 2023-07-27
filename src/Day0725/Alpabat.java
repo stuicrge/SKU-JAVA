9b3789418cb497af27e7bdd75e85f818a58456c2
tiran <tiran@DESKTOP-UG90DKO>
2023-07-27 22:27:07 +0900

    hel

--- src/Day0725/Alpabat.java
+++ src/Day0725/Alpabat.java
@@ -5,6 +5,7 @@ public class Alpabat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int j = 0;
 		
 		StringBuilder stringbuilder = new StringBuilder();
 		
